3a306bed4d4cb1f8a70e31657b8698faf995b52a
Ashish-3001 <cvnnashish@gmail.com>
2020-07-05 23:55:46 +0530

    update

--- database/hiree_database/database_app/views.py
+++ database/hiree_database/database_app/views.py
@@ -19,8 +19,36 @@ from .serializer import ShortListedSerializers
 from .serializer import EmployerDetailsFavSerializers
 from .serializer import EmployeeDetailsFavSerializers
 from rest_framework import filters
+from rest_framework import generics
+from django_filters import rest_framework as FilterSet
 import django_filters.rest_framework
 # Create your views here.
+
+class JobPostFilterSet(django_filters.FilterSet):
+    negated_job_salary = django_filters.Filter('job_salary', exclude=True)
+    negated_eyer_location = django_filters.Filter('eyer_location', exclude=True)
+    negated_job_gender = django_filters.Filter('job_gender', exclude=True)
+    negated_job_education = django_filters.Filter('job_education', exclude=True)
+    negated_job_experience = django_filters.Filter('job_experience', exclude=True)
+    negated_job_age = django_filters.Filter('job_age', exclude=True)
+
+    class Meta:
+        model = JobPost
+        fields = ['eyer_id', 'job_active','job_post','job_salary','eyer_location','job_gender','job_education','job_experience','job_age']
+
+class EmployeeDetailsFilterSet(django_filters.FilterSet):
+    negated_eyee_salary_expected = django_filters.Filter('eyee_salary_expected', exclude=True)
+    negated_eyee_pre_experience = django_filters.Filter('eyee_pre_experience', exclude=True)
+    negated_eyee_education = django_filters.Filter('eyee_education', exclude=True)
+    negated_eyee_address_2 = django_filters.Filter('eyee_address_2', exclude=True)
+    negated_eyee_type_hotel = django_filters.Filter('eyee_type_hotel', exclude=True)
+    negated_eyee_age = django_filters.Filter('eyee_age', exclude=True)
+    negated_eyee_gender = django_filters.Filter('eyee_gender', exclude=True)
+
+    class Meta:
+        model = EmployeeDetails
+        fields = ['user_id','eyee_choice','eyee_salary_expected','eyee_pre_experience','eyee_education','eyee_city','eyee_type_hotel','eyee_age','eyee_gender']
+
 
 class UserLoginViewSet(viewsets.ModelViewSet):
 
@@ -40,13 +68,18 @@ class EmployeeDetailsViewSet(viewsets.ModelViewSet):
     queryset = EmployeeDetails.objects.all()
     serializer_class = EmployeeDetailsSerializers
     filter_backends = (django_filters.rest_framework.DjangoFilterBackend,filters.OrderingFilter,)
-    filter_fields = ('user_id',)
+    filter_fields = ('user_id','eyee_choice','eyee_salary_expected','eyee_pre_experience','eyee_education','eyee_city','eyee_type_hotel','eyee_age','eyee_gender',)
+    ordering_fields = '__all__'
+    filter_class = EmployeeDetailsFilterSet
 
 class JobPostViewSet(viewsets.ModelViewSet):
     queryset = JobPost.objects.all()
     serializer_class = JobPostSerializers
-    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,filters.OrderingFilter,)
-    filter_fields = ('eyer_id', 'job_active')
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,filters.OrderingFilter,filters.SearchFilter)
+    filter_fields = ('eyer_id', 'job_active','job_post','job_salary','eyer_location','job_gender','job_education','job_experience','job_age')
+    search_fields = ('eyer_name','eyer_location','job_post')
+    ordering_fields = '__all__'
+    filter_class = JobPostFilterSet
 
 class JobOfferViewSet(viewsets.ModelViewSet):
     queryset = JobOffer.objects.all()
